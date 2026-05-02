574b66e77cc8ac8df46a81943a35e6fbfc9a54f0
danielcch <mapimail@gmail.com>
2026-05-02 23:18:53 +0200

    Initial Commit

---
+++ Proyecto1/Proyecto1App.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto1App.swift
+//  Proyecto1
+//
+//  Created by DANIEL on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
