70c9944a866b3011e6afeaf1a7c6d91bcf94f33a
danielcch <mapimail@gmail.com>
2026-05-03 00:30:42 +0200

    t

--- Tuist.swift
+++ Tuist.swift
@@ -1,17 +1,9 @@
-import Foundation
 import ProjectDescription
 
-func remoteHandle() -> String? {
-    let handle = Environment.fullHandle.getString(default: "").trimmingCharacters(in: .whitespacesAndNewlines)
-    return handle.isEmpty ? nil : handle
-}
-
 let tuist = Tuist(
-    fullHandle: remoteHandle(),
     project: .tuist(
         generationOptions: .options(
             defaultConfiguration: "Debug",
-            optionalAuthentication: true,
             additionalPackageResolutionArguments: ["-scmProvider", "system"]
         ),
         cacheOptions: .options(
